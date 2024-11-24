4c939e3b8215a70c12b8934ecfba8da2b5235096
Mopsior <bartek.szg@gmail.com>
2024-11-24 13:11:33 +0100

    Uncomment security checks

--- app/setup/layout.tsx
+++ app/setup/layout.tsx
@@ -1,17 +1,17 @@
 import { Separator } from '@/components/ui/separator';
 import styles from '@/styles/setup/styles.module.css'
-// import { catchError } from '@/utils/catch-error';
-// import { db } from '@/utils/firebase/firebaseServer';
-// import { redirect } from 'next/navigation';
+import { catchError } from '@/utils/catch-error';
+import { db } from '@/utils/firebase/firebaseServer';
+import { redirect } from 'next/navigation';
 
 export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    // const [checkError, checkData] = await catchError(db.collection('config').doc('settings').get())
-    // if (checkError) return redirect('/login')
-    // if (checkData.data()?.appAfterSetup) return redirect('/login')
+    const [checkError, checkData] = await catchError(db.collection('config').doc('settings').get())
+    if (checkError) return redirect('/login')
+    if (checkData.data()?.appAfterSetup) return redirect('/login')
     return (
         <>
             <div className={styles.title}>
